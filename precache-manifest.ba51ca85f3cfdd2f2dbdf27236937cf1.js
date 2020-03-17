147201526cc27f8d07fe3fd180965c75c4fa29ab
Alexis <qq_wei369@hotmail.com>
2020-03-17 09:48:59 -0700

    Updates

---
+++ precache-manifest.ba51ca85f3cfdd2f2dbdf27236937cf1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4b1cd1b09d82e4e2a3875bd3087e10d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "045aac1cedbf6a53ec63",
+    "url": "/static/css/2.7ad897a5.chunk.css"
+  },
+  {
+    "revision": "ec277d217c6ec7a74796",
+    "url": "/static/css/main.415a8fa7.chunk.css"
+  },
+  {
+    "revision": "045aac1cedbf6a53ec63",
+    "url": "/static/js/2.4e4c6b76.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/static/js/2.4e4c6b76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec277d217c6ec7a74796",
+    "url": "/static/js/main.dbf328a5.chunk.js"
+  },
+  {
+    "revision": "3ad0a8cae43cdb4cec55",
+    "url": "/static/js/runtime-main.9f47148d.js"
+  },
+  {
+    "revision": "eeb2b83be45318b308f6864741a0e98c",
+    "url": "/static/media/Big-Ideas-logo.eeb2b83b.png"
+  },
+  {
+    "revision": "c556ef06e53272d41076eaa1911256ad",
+    "url": "/static/media/Legacy1.c556ef06.svg"
+  },
+  {
+    "revision": "cb502125e023f4a8157c52d340d41444",
+    "url": "/static/media/berk-law.cb502125.jpg"
+  }
+]);
